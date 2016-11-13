8e4749bff95272858f574da48bd37fec99a079c3
Kevin Chau <kchau.jobs@gmail.com>
2016-11-13 11:51:49 -0800

    controller and http

--- controller.js
+++ controller.js
@@ -2,15 +2,28 @@
  * Created by kevin on 11/13/16.
  */
 var app = angular.module('sgtApp', [])
-.controller('sgtController', function($log){
-    this.studentList = [];
-    this.student = {};
-}).service('studentService', function($http, $log){
-    this.getStudents = function(){
-        $http({
-           method: 'GET',
-           url:
-        dat
-        });
-    }
+    .controller('sgtController', function($log){
+        this.studentList = [];
+        this.student = {};
+    })
+    .service('studentService', function($http, $log){
+        this.data = {'api_key':'Mmkxjt1mxT'};
+        this.getStudents = function(){
+            var list;
+            $http({
+                method: 'POST',
+                url: 'https://s-apis.learningfuze.com/sgt/get',
+                dataType: 'JSON',
+                data: this.data
+            })
+                .then(
+                    function(response){
+                        $log.info('Success: ', response);
+                        list = response;
+                    },
+                    function(error){
+                        $.log.warn('Failure: ', error);
+                        list = error;
+                    });
+        }
     });
