e1fd98cc015891317c2d08626c9235e7ff9dda55
Kevin Chau <kchau.jobs@gmail.com>
2016-11-13 16:58:20 -0800

    http not working

--- controller.js
+++ controller.js
@@ -2,9 +2,17 @@
  * Created by kevin on 11/13/16.
  */
 var app = angular.module('sgtApp', [])
-    .controller('sgtController', function($log){
-        this.studentList = [];
+    .controller('sgtController', function($log,studentService){
+        this.studentList = studentService.getStudents();
         this.student = {};
+        this.addClicked = function(){
+            //this.studentList.push(this.student);
+            $log.log(this.studentList);
+            this.student = {};
+        };
+        this.cancelClicked = function(){
+            this.student = {};
+        };
     })
     .service('studentService', function($http, $log){
         this.data = {'api_key':'Mmkxjt1mxT'};
@@ -13,7 +21,7 @@ var app = angular.module('sgtApp', [])
             $http({
                 method: 'POST',
                 url: 'https://s-apis.learningfuze.com/sgt/get',
-                dataType: 'JSON',
+                dataType: 'JSONP',
                 data: this.data
             })
                 .then(
@@ -25,5 +33,8 @@ var app = angular.module('sgtApp', [])
                         $.log.warn('Failure: ', error);
                         list = error;
                     });
-        }
+        };
+        this.addStudent = function(){
+
+        };
     });
