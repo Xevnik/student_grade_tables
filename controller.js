07121f98f562d2e79ba90a55917019cb74afe344
Kevin Chau <kchau.jobs@gmail.com>
2016-11-14 15:10:48 -0800

    populates on page load

--- controller.js
+++ controller.js
@@ -1,40 +1,66 @@
 /**
  * Created by kevin on 11/13/16.
  */
-var app = angular.module('sgtApp', [])
-    .controller('sgtController', function($log,studentService){
-        this.studentList = studentService.getStudents();
-        this.student = {};
-        this.addClicked = function(){
-            //this.studentList.push(this.student);
-            $log.log(this.studentList);
-            this.student = {};
-        };
-        this.cancelClicked = function(){
-            this.student = {};
-        };
-    })
-    .service('studentService', function($http, $log){
-        this.data = {'api_key':'Mmkxjt1mxT'};
-        this.getStudents = function(){
-            var list;
-            $http({
-                method: 'POST',
-                url: 'https://s-apis.learningfuze.com/sgt/get',
-                dataType: 'JSONP',
-                data: this.data
-            })
-                .then(
-                    function(response){
-                        $log.info('Success: ', response);
-                        list = response;
-                    },
-                    function(error){
-                        $.log.warn('Failure: ', error);
-                        list = error;
-                    });
-        };
-        this.addStudent = function(){
+var app = angular.module('sgtApp', []);
 
-        };
-    });
+app.provider('studentData', function(){
+    var dataScope = this;
+    dataScope.apiUrl = '';
+    dataScope.apiKey = '';
+    dataScope.$get = function($http, $q, $log){
+        return {
+            callApi: function(){
+                var data = 'api_key=' + dataScope.apiKey;
+                var defer = $q.defer();
+                $http({
+                    method: 'POST',
+                    url: dataScope.apiUrl,
+                    dataType: 'JSON',
+                    data: data,
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                })
+                    .then(
+                        function(resp){
+                            $log.info('success');
+                            defer.resolve(resp);
+                        },
+                        function(err){
+                            $log.warn('error');
+                            defer.reject(err);
+                        });
+                return defer.promise;
+            }
+        }
+    };
+});
+
+app.config(function(studentDataProvider){
+    studentDataProvider.apiUrl = 'https://s-apis.learningfuze.com/sgt/get';
+    studentDataProvider.apiKey = 'Mmkxjt1mxT';
+});
+
+app.controller('sgtController', function($log, $window, studentData){
+    var scScope = this;
+    scScope.student = {};
+    scScope.studentList = [];
+    $window.onload = function() {
+        $log.log('In update');
+        studentData.callApi()
+            .then(
+                function (response) {
+                    $log.info('Success: ', response);
+                    scScope.studentList = response.data.data;
+                },
+                function (error) {
+                    $log.warn('Failure: ', error);
+                });
+    };
+    scScope.addClicked = function(){
+        //this.studentList.push(this.student);
+        $log.log(this.studentList);
+        scScope.student = {};
+    };
+    scScope.cancelClicked = function(){
+        scScope.student = {};
+    };
+});
