28c440bd345563d17828188b7d614bd73488295d
Kevin Chau <kchau.jobs@gmail.com>
2017-01-15 18:10:09 -0800

    converted to use angularfire

--- config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by kevin on 12/28/16.
- */
-app.config(function(studentDataProvider){
-    studentDataProvider.apiUrl = 'https://s-apis.learningfuze.com/sgt/';
-    studentDataProvider.apiKey = 'Mmkxjt1mxT';
-});
